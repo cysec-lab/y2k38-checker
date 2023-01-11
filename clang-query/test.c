46196739d8307ea4d289a8b475e7b8a5742ea2b2
Hoshi Ran <ureshinostyle350@gmail.com>
2023-01-11 22:35:41 +0900

    feat: readバグ用の matcher を作成

--- clang-query/test.c
+++ clang-query/test.c
@@ -12,6 +12,7 @@ void referFileTimeStamp() {
         printf("%ld\n", statBuf.st_atime);
         printf("%ld\n", statBuf.st_mtime);
         printf("%ld\n", statBuf.st_ctime);
+        printf("%ld\n", statBuf.st_mode);  // st_[amc]time 以外はマッチされない
     }
 }
 
