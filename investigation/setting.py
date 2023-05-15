4cc62081cbcf1eeeedced0d7d87839d14f0cc20a
Hoshi Ran <ureshinostyle350@gmail.com>
2023-05-16 04:01:37 +0900

    feat: 検証前処理用 python スクリプト

---
+++ investigation/setting.py
@@ -0,0 +1,18 @@
+repo = "/Users/rannosukehoshina/Develop/y2k38-checker"
+TARGET_DIR_PATH = repo + "/target"
+ANALYZED_DIR_PATH = repo + "/analyzed"
+COMMAND_COMPILE_DIR_PATH = repo + "/compile_commands"
+RUN_SHELL_DIR_PATH = repo + "/run"
+
+# compile_commands.json 用
+CLANG_PATH = "あとで追記"
+TOOL_RUN_DIRECTORY = "あとで追記"
+
+TOOL_PATH = "あとで追記"
+
+TARGET_ZIP_URLS = [
+    { 
+        "name": "krakjoe-phpdbg", 
+        "url":  "https://github.com/krakjoe/phpdbg/archive/refs/heads/master.zip" 
+    }
+]
