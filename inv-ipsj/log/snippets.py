531aad676c829943f96b74bc43482f22ac135dc6
Hoshi Ran <ureshinostyle350@gmail.com>
2023-11-25 13:27:29 +0900

    検出数ランキングを作成した

--- inv-ipsj/log/snippets.py
+++ inv-ipsj/log/snippets.py
@@ -1,17 +1,25 @@
-import pandas as pd
+# analysis.csv
 
+
+import pandas as pd
 df = pd.read_csv("analysis.csv")
 
 df["count_files"]
 df["count_files"].sum()
+
+# analysis_detail.csv
+
+import pandas as pd
+df = pd.read_csv("analysis_detail.csv")
+a = df[-df["path"].str.startswith("/home/cysec/develop/y2k38-checker/inv-ipsj/analyze")]
+
+category = a[a["y2k38_category"] == "timet-to-int-downcast"]
+category = a[a["y2k38_category"] == "timet-to-long-downcast"]
+category = a[a["y2k38_category"] == "read-fs-timestamp"]
+category = a[a["y2k38_category"] == "write-fs-timestamp"]
+
+a["analysis_id"].drop_duplicates()
 a["path"].to_csv("path.log", index=False, header=False)
 
-
-df = pd.read_csv("analysis_detail.csv")
-
-a = df[-df["path"].str.startswith("/home/cysec/develop/y2k38-checker/inv-ipsj/analyze")]
-a[a["y2k38_category"] == "read-fs-timestamp"]
-a[a["y2k38_category"] == "write-fs-timestamp"]
-a[a["y2k38_category"] == "timet-to-int-downcast"]
-a[a["y2k38_category"] == "timet-to-long-downcast"]
-a["analysis_id"].drop_duplicates()
+# 範囲を指定してカウント
+grouped = category.groupby("analysis_id").size().sort_values(ascending=False).reset_index(name='size')
